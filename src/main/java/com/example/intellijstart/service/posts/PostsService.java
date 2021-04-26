742d842ff024c6efb4e21d25d24c0b1255fd6966
newbie-newb <userA@my_email.com>
2021-04-26 22:08:02 +0900

    crud 구현

--- src/main/java/com/example/intellijstart/service/posts/PostsService.java
+++ src/main/java/com/example/intellijstart/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.example.intellijstart.service.posts;
 
 import com.example.intellijstart.domain.posts.Posts;
 import com.example.intellijstart.domain.posts.PostsRepository;
+import com.example.intellijstart.web.dto.PostsListResponseDto;
 import com.example.intellijstart.web.dto.PostsResponseDto;
 import com.example.intellijstart.web.dto.PostsSaveRequestDto;
 import com.example.intellijstart.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -46,6 +50,11 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
-
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 
 }
